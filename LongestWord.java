6d7841b4cc87ce67cf651b3b54bb6b76c2783803
yashvir <yashvir.singh_cs18@gla.ac.in>
2020-05-12 00:13:52 -0700

    Scanner class

--- LongestWord.java
+++ LongestWord.java
@@ -1,4 +1,9 @@
+import java.util.Scanner;
+
 public class LongestWord {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] s = sc.nextLine().split(" ");
+        int ele = 0;
 
     }
