8d98293ce9071c7b943be62f91cbacdf09441198
yashvir <yashvir.singh_cs18@gla.ac.in>
2020-05-12 00:14:35 -0700

    program complete

--- LongestWord.java
+++ LongestWord.java
@@ -5,5 +5,13 @@ public class LongestWord {
         Scanner sc = new Scanner(System.in);
         String[] s = sc.nextLine().split(" ");
         int ele = 0;
+        for (int i = 0; i < s.length; i++) {
+            if (s[i].length() > s[ele].length()) {
+                ele = i;
+            }
+        }
+        System.out.println(s[ele]);
 
     }
+
+}
